d004652b9042a5b574972376e8bff29aa7c0191c
Raufnarejo505 <arauf.bscsses21@iba-suk.edu.pk>
2022-04-13 09:40:46 -0700

    dfsd

---
+++ DSLab-02/src/Exam.java
@@ -0,0 +1,50 @@
+import java.util.LinkedList;
+
+public class Exam {
+    //    Stack<Integer> stack = new Stack<Integer>();
+    public static void main(String[] args) {
+//    Stack<Integer> stack1 = new Stack<Integer>();
+//    stack1.push(4);
+//    stack1.push(6);
+//    stack1.push(8);
+//    stack1.push(3);
+//    stack1.push(2);
+//    stack1.push(5);
+//    Stack<Integer> stack2 = new Stack<Integer>();
+//    int numb= 0 ;
+//    while(!stack1.isEmpty()){
+//        if(stack1.peek()>numb){
+//            numb=stack1.peek();
+//            stack2.push(stack1.pop());
+//        }else{
+//            stack2.push(stack1.pop());
+//        }
+//    }
+//    while(!stack2.isEmpty()){
+//        if(stack2.peek()==numb){
+//            stack2.pop();
+//        }else{
+//            stack1.push(stack2.pop());
+//        }
+//    }
+//    while(!stack1.isEmpty()){
+//        System.out.println(stack1.pop());
+//    }
+//}
+        LinkedList<Integer> LL = new LinkedList<Integer>();
+        LL.add(1);
+        LL.add(2);
+        LL.add(3);
+        LL.add(4);
+        LL.add(5);
+        LinkedList<Integer> LL2 = new LinkedList<Integer>();
+        LL2.add(6);
+        LL2.add(7);
+        LL2.add(8);
+        LL2.add(9);
+        LL2.add(10);
+        LinkedList<Integer> LL3 = new LinkedList<Integer>();
+
+
+    }
+}
